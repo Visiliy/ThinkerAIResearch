9d2edc28ef4fd3c629cefb301a96a77b13b4b6ca
Visiliy <vasiliyshkuratov_888@mail.ru>
2025-08-10 11:43:30 +0300

    /

---
+++ NewTransformer.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class Decoder(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
