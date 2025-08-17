43d86e20f19417ee8c1b14b35142267b389b3ea5
Visiliy <vasiliyshkuratov_888@mail.ru>
2025-08-17 22:36:57 +0300

    /

--- test.py
+++ test.py
@@ -15,18 +15,20 @@ class Influence(nn.Module):
         self.out_linear2 = nn.Linear(embed_dim, embed_dim)
         self.out_linear3 = nn.Linear(embed_dim, embed_dim)
 
+    def apply_permute(self, tensor_lists):
+        new_tensor_lists = []
+        for tensor, one, two, three in tensor_lists:
+            tensor = tensor.permute(one, two, three)
+            new_tensor_lists.append(tensor)
+        return new_tensor_lists
+
     def forward(self, Q, K, V):
-        Q = Q.permute(2, 0, 1)
-        K = K.permute(2, 0, 1)
-        V = V.permute(2, 0, 1)
+        Q, K, V = self.apply_permute([[Q, 2, 0, 1], [K, 2, 0, 1], [V, 2, 0, 1]])
 
         Q = self.linear1(Q)
         K = self.linear2(K)
         V = self.linear3(V)
 
-        Q = Q.permute(1, 2, 0)
-        K = K.permute(1, 2, 0)
-        V = V.permute(1, 2, 0)
-
+        Q, K, V = self.apply_permute([[Q, 1, 2, 0], [K, 1, 2, 0], [V, 1, 2, 0]])
 
         return self.out_linear1(Q), self.out_linear2(K), self.out_linear3(V)
