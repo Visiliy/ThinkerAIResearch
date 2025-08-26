bc2ab862f873ac77f13055017a2e0cc2998bd9c1
Visiliy <vasiliyshkuratov_888@mail.ru>
2025-08-26 12:12:16 +0200

    /

--- ConvBlockModel.py
+++ ConvBlockModel.py
@@ -1,6 +1,4 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from ConvBlock import DynamicPooling, LinearPerformerAttention, LinearParameterizationKernel, FastKernelCompression, LinearBlockSparseAttention, OptimizedDilatedResidual, TokenMerging, LinearLocalAttention, LinearDynamicInceptionBlock
 
 
@@ -18,18 +16,20 @@ class ConvBlock(nn.Module):
         self.local_attention = LinearLocalAttention(dim, window_size=7, heads=heads, feature_dim=64, dropout=dropout)
         self.dynamic_inception_block = LinearDynamicInceptionBlock(dim, kernel_sizes=[1, 3, 5], feature_dims=[32, 64, 32], dropout=dropout)
 
-        total_output_dim = (dim // 3) * 4
-        self.final_norm = nn.LayerNorm(total_output_dim)
+        self.inception_proj = nn.Linear((dim // 3) * 4, dim)
+
+        self.final_norm = nn.LayerNorm(dim)
         
     def forward(self, x):
-        x = self.dynamic_pooling(x)
-        x = self.performer_attention(x)
-        x = self.parameterization_kernel(x)
-        x = self.kernel_compression(x)
-        x = self.block_sparse_attention(x)
-        x = self.dilated_residual_cnn(x)
-        x = self.token_merging(x)
-        x = self.local_attention(x)
         x = self.dynamic_inception_block(x)
+        x = self.inception_proj(x)
+        x = self.local_attention(x)
+        x = self.token_merging(x)
+        x = self.dilated_residual_cnn(x)
+        x = self.block_sparse_attention(x)
+        x = self.kernel_compression(x)
+        x = self.parameterization_kernel(x)
+        x = self.performer_attention(x)
+        x = self.dynamic_pooling(x)
         
         return self.final_norm(x)
